55f65d6d1628e90e4f91825f24860191ef84f645
nepile <nevillejro.laia@gmail.com>
2023-03-11 08:10:07 +0700

    update web route and some controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController as Auth;
+use App\Http\Controllers\Dash\BeritaController as Berita;
+use App\Http\Controllers\Dash\DataLaporanController as DataLaporan;
+use App\Http\Controllers\Dash\ManagementUserController as ManagementUser;
 use App\Http\Controllers\LandingPage\LandingPageController as LandingPage;
-use App\Http\Controllers\OverviewController as Overview;
+use App\Http\Controllers\Dash\OverviewController as Overview;
+use App\Http\Controllers\PengaturanController;
 use Illuminate\Support\Facades\Route;
 
 // landing page
@@ -14,3 +18,10 @@ Route::get('/login', [Auth::class, 'v_login'])->name('login');
 
 // dashboard
 Route::get('/overview', [Overview::class, 'index'])->name('overview');
+Route::get('/data_laporan', [DataLaporan::class, 'v_dataLaporan'])->name('data_laporan');
+Route::middleware('role:admin')->prefix('/management_user')->group(function () {
+    Route::get('/management_masyarakat', [ManagementUser::class, 'v_masyarakat'])->name('management_masyarakat');
+    Route::get('/management_petugas', [ManagementUser::class, 'v_petugas'])->name('management_petugas');
+});
+Route::get('/berita', [Berita::class, 'v_berita'])->name('berita');
+Route::get('/pengaturan', [PengaturanController::class, 'v_pengaturan'])->name('pengaturan');
