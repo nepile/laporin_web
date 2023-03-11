cfea6fcd1d62fa1673e677730e9c687d615144fc
nepile <nevillejro.laia@gmail.com>
2023-03-11 20:57:53 +0700

    add migration table: laporan, tanggapan, berita and update users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('telp');
+            $table->enum('role', ['masyarakat', 'petugas', 'admin']);
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
