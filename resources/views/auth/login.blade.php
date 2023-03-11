b9d324c1e4056bf5ee386a42fa2aff17a2501d40
nepile <nevillejro.laia@gmail.com>
2023-03-11 22:47:27 +0700

    update view auth/login and add auth template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,68 @@
-@extends('layouts.app')
+@extends('templates.auth')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+@section('auth-content')
+<div class="container-fluid">
+    <div class="row bg-dark" style="height: 100vh;">
+        <div class="col-xl-4 p-4" style="height: 100%; background: #fff;">
+            <a href="{{ route('home') }}" style="text-decoration: none; color: #000">
+                <div class="col-12 d-flex align-items-center">
+                    <img src="{{ asset('img/logo2.png') }}" style="width: 70px;">
+                    <h5 class="ms-2" style="font-weight: bold;">Laporin</h5>
+                </div>    
+            </a>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            @auth
+            <div class="mt-5">
+                <h4 style="font-weight: bold">Aksi Ditolak!</h4>
+                <p>Silakan logout terlebih dahulu.</p>
+    
+                <form action="/handle_logout" method="POST">
+                    @csrf
+                    <button class="btn btn-danger">Logout</button>
+                </form>
             </div>
+
+            @else
+            <div class="col-xl-9 col-12 mt-5">
+                <h5 style="font-weight: bold;" class="mb-0">Selamat Datang!</h5>
+                <p style="font-size: 13px;">Silakan login terlebih dahulu untuk mendapatkan akses.</p>
+            </div>
+
+            <div class="col-xl-12 mt-4">
+                <form action="/handle_login" method="POST">
+                    @csrf
+                    <div class="mt-3">
+                        <label for="username" style="font-weight: 500;">Username:</label>
+                        <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="username" value="{{ old('username') }}" style="border-radius: 5px; height: 50px;" placeholder="Masukkan username anda">
+                        @error('username')
+                            <span class="text-danger" style="font-size: 12px">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mt-4">
+                        <label for="password" style="font-weight: 500;">Password:</label>
+                        <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" style="border-radius: 5px; height: 50px;" placeholder="Masukkan password anda">
+                        @error('password')
+                            <span class="text-danger" style="font-size: 12px">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mt-5">
+                        <button class="btn btn-dark" type="submit" style="width: 100%; color: #fff; height: 50px;">
+                            Masuk
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            @endauth
+
+        </div>
+
+        <div class="col-xl-8 p-0 d-xl-block d-none" style="height: 100%;">
+            <img src="{{ asset('img/login_background.jpg') }}" style="height: 100%; width: 100%; object-fit: cover;">
         </div>
     </div>
 </div>
+
 @endsection
