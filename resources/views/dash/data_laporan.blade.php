6c455e3c1441d5bbfe7f6ad48ac76b97303418ca
nepile <nevillejro.laia@gmail.com>
2023-03-11 21:43:38 +0700

    update data_laporan view

--- resources/views/dash/data_laporan.blade.php
+++ resources/views/dash/data_laporan.blade.php
@@ -1,5 +1,33 @@
 @extends('templates.dash')
 
 @section('dash-content')
-halaman management data laporan
+    <div class="container-fluid mb-5">
+        <div class="row bg-dark justify-content-center align-items-center" style="height: 45vh">
+            <div class="col-12 mt-5 text-light p-4">
+                <h4 class="mb-0" style="font-weight: 700">
+                    {{ $title }}
+                </h4>
+                <p>
+                    <a href="{{ route('overview') }}" class="text-light">
+                        <i class="fa fa-home"></i> Dashboard
+                    </a> /
+                    <span>{{ $title }}</span>
+                </p>
+            </div>
+        </div>
+
+        <div class="row" style="position: relative; bottom: 50px;">
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-header">
+                        mm
+                    </div>
+                    <div class="card-body">
+                        mm
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 @endsection
