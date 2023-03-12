032629453d510f234fcbe1b1d21e6cffe72c94c2
nepile <nevillejro.laia@gmail.com>
2023-03-12 08:06:07 +0700

    update login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller
         );
 
         if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
             return redirect()->route('overview');
         }
 
