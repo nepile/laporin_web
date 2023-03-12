bb9f74002302fdbd440e519684e636ba7dff4517
nepile <nevillejro.laia@gmail.com>
2023-03-12 21:08:57 +0700

    update berita

--- resources/views/dash/berita.blade.php
+++ resources/views/dash/berita.blade.php
@@ -1,5 +1,53 @@
 @extends('templates.dash')
 
 @section('dash-content')
-halaman berita
+    <div class="container-fluid mb-5">
+        <div class="row bg-dark justify-content-center align-items-center" style="height: 45vh">
+            <div class="col-12 mt-5 text-light p-4">
+                <h4 class="mb-0" style="font-weight: 700">
+                    {{ $title }}
+                </h4>
+                <p>
+                    <a href="{{ route('overview') }}" class="text-light">
+                        <i class="fa fa-home"></i> Dashboard
+                    </a> /
+                    <span>{{ $title }}</span>
+                </p>
+            </div>
+        </div>
+        <div class="row" style="position: relative; bottom: 50px;">
+            <div class="col-xl-12">                
+                @include('components.alert')
+                <div class="card">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                          <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                              Accordion Item #1
+                            </button>
+                          </h2>
+                          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                          </div>
+                        </div>
+                        <div class="accordion-item">
+                          <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                              <strong>Tambah Berita</strong>
+                            </button>
+                          </h2>
+                          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 @endsection
